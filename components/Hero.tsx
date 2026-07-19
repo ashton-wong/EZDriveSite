2614cdffbb45d5c7644acb8a8825f53d06d56c05
Connor-Udolph <connorudolph@gmail.com>
2026-07-18 17:38:10 -0700

    Change button text from 'EZDrive' to 'MotorLink'

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -150,7 +150,7 @@ export default function Hero() {
         }}
       >
         <a className="btn btn-primary" href="#preorder">
-          Get EZDrive →
+          Get MotorLink →
         </a>
         <a className="btn btn-secondary" href="#features">
           See how it works
